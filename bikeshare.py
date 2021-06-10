6b5cb9bb2954ef50c7613459c8ea4c42b0cef3a3
KhaliidMn <khalid.m.alnahed@gmail.com>
2021-06-10 21:10:20 +0300

    printing row data feature has been added

--- bikeshare.py
+++ bikeshare.py
@@ -163,6 +163,13 @@ def user_stats(df):
     
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
+    
+    view_data = input('\nWould you like to view 5 rows of individual trip data? Enter yes or no\n')
+    start_loc = 0
+    while (view_data=='yes'):
+        print(df.iloc[start_loc:start_loc+5])
+        start_loc += 5
+        view_data = input("Do you wish to continue?: ").lower()
 
 
 def main():
